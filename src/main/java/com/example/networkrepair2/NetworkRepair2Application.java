9fee82065906a3223aceba7788bfb0c1d40a39b4
WOOGUGU <WOOGUGU@OUTLOOK.com>
2022-03-02 14:34:29 +0800

    基础框架

---
+++ src/main/java/com/example/networkrepair2/NetworkRepair2Application.java
@@ -0,0 +1,13 @@
+package com.example.networkrepair2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkRepair2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkRepair2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/networkrepair2/NetworkRepair2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.networkrepair2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkRepair2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
